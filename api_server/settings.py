eb0aafd0c3b328abd5148b40db398f2e93831441
wafflesLL <lloeffel@vols.utk.edu>
2025-10-01 20:07:26 +0000

    fixed debugging changes to be actual settings

--- api_server/settings.py
+++ api_server/settings.py
@@ -20,7 +20,7 @@ INSTALLED_APPS = [
 ]
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
