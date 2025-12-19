57e55b758e041dfbea910280acfa275b3bb457f2
wafflesLL <lloeffel@vols.utk.edu>
2025-12-18 23:57:33 -0500

    got it to recognize my nose based on a given range of values

--- getfeatures.py
+++ getfeatures.py
@@ -1,46 +1,57 @@
 #!/usr/bin/env python3
 import numpy as np
 import open3d as o3d
-from scipy.signal import find_peaks
 
 
-def find_nose_tip(points, prominence=5.0, width=10.0):
+def define_search_space(points,
+                        x_range,
+                        y_range,
+                        z_range):
+    x_mask = (points[:, 0] >= x_range[0]) & (points[:, 0] <= x_range[1])
+    y_mask = (points[:, 1] >= y_range[0]) & (points[:, 1] <= y_range[1])
+    z_mask = (points[:, 2] >= z_range[0]) & (points[:, 2] <= z_range[1])
+    mask = x_mask & y_mask & z_mask
+    out = points[mask]
+
+    return out
+
+
+def find_closest_point(points):
     z = points[:, 2]
 
-    peaks, properties = find_peaks(z, prominence=prominence, width=width)
+    best_idx = np.argmax(z)
 
-    if len(peaks) == 0:
-        print("No distinct nose bump was found (Face might be very flat)")
-        return None, None
-
-    best_idx = np.argmax(properties['prominences'])
-    peak_index = peaks[best_idx]
-
-    idx_start = int(properties['left_ips'][best_idx])
-    idx_end = int(properties['right_ips'][best_idx])
-
-    nose_peak_point = points[peak_index]
-    y_start = points[idx_start, 1]
-    y_end = points[idx_end, 1]
-
-    print(f"Nose found between Y={y_start:.1f} and Y={y_end:.1f}")
-
-    return nose_peak_point, (y_start, y_end)
+    nose_peak_point = points[best_idx]
+    return nose_peak_point
 
 
 def main():
+    x_range = (-5.0, 5.0)
+    y_range = (-2.0, 2.0)
+    z_range = (-10.0, 0.0)
     pcd = o3d.io.read_point_cloud('./data/liams-face.ply')
     ori_points = np.asarray(pcd.points)
     points = ori_points
 
-    nose_peak, nose_range = find_nose_tip(points, prominence=5.0, width=5.0)
-    mesh_sphere = o3d.geometry.TriangleMesh.create_sphere(radius=1)
-    mesh_sphere.translate(nose_peak)
-    mesh_sphere.paint_uniform_color([1, 0, 0])
+    def debug_blob(point, size):
+        sphere = o3d.geometry.TriangleMesh.create_sphere(radius=size)
+        sphere.translate(point)
+        sphere.paint_uniform_color([1, 0, 0])
 
-    sphere_pcd = mesh_sphere.sample_points_uniformly(number_of_points=500)
-    o3d.io.write_point_cloud("./data/debug_output.ply", sphere_pcd)
+        sphere_pcd = sphere.sample_points_uniformly(number_of_points=500)
+        return sphere_pcd
 
+    subspace = define_search_space(points, x_range, y_range, z_range)
+
+    max_range = debug_blob(np.asarray([x_range[1], y_range[1], z_range[1]]),
+                           0.5)
+    min_range = debug_blob(np.asarray([x_range[0], y_range[0], z_range[0]]),
+                           0.5)
+    nose_blob = debug_blob(find_closest_point(subspace), 1)
+
+    debug_pcd = max_range + min_range + nose_blob
+
+    o3d.io.write_point_cloud("./data/debug_output.ply", debug_pcd)
     return
 
 
